f47e8c9b2e589f07b3730861f3320cd492b9c778
Adrian <adrian.elrahi@gmail.com>
2026-06-28 09:44:57 +0300

    "Corrected Prisma Setup"

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -2,7 +2,5 @@ import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  datasource: {
-  url: process.env["DATABASE_URL"]
-}
+  schema: "prisma/schema.prisma"
 });
